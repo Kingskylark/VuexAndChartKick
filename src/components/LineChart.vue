b6fa0dfcecfde955f05cade5585d0053ea1800b0
Kopuko King <whisper2ku@gmail.com>
2022-06-17 19:20:16 +0100

    Update LineChart.vue

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,34 +1,34 @@
 <template>
   <div class="home">
+  
     <div>
       <div>
         <h4>Bar Chart</h4>
-        <bar-chart :data="data"></bar-chart>
+        <bar-chart :data="chartData.map(i => [i.date, i.total])"></bar-chart>
       </div>
       <div>
         <h4>Line Chart</h4>
-        <line-chart :data="data"></line-chart>
+        <line-chart :data="chartData.map(i => [i.date, i.total])"></line-chart>
       </div>
       <div>
         <h4>Pie Chart</h4>
-        <pie-chart :data="data"></pie-chart>
+        <pie-chart :data="chartData.map(i => [i.date, i.total])"></pie-chart>
       </div>
       <div>
         <h4>Area Chart</h4>
-        <area-chart :data="data"></area-chart>
+        <area-chart :data="chartData.map(i => [i.date, i.total])"></area-chart>
       </div>
       <div>
         <h4>Column Chart</h4>
-        <column-chart :data="data"></column-chart>
+        <column-chart :data="chartData.map(i => [i.date, i.total])"></column-chart>
       </div>
       <div>
         <h4>Scatter Chart</h4>
-        <scatter-chart :data="data"></scatter-chart>
+        <scatter-chart :data="chartData.map(i => [i.date, i.total])"></scatter-chart>
       </div>
     </div>
   </div>
 </template>
-
 
 
 <script>
@@ -43,10 +43,10 @@ export default {
 
   data() {
     return {
-      // chartData:[],
-      data: {},
+       data: {},
     };
   },
+
   computed: {
     ...mapState([
       "arrPositive",
@@ -55,22 +55,12 @@ export default {
       "arrOnVentilators",
       "arrDeath",
     ]),
-  },
+     },
 
-  methods: {
-    getValues() {
-      const values = this.chartData.map((item) => ({
-        [item.date]: item.total,
-      }));
-      var newObj = Object.assign({}, ...values);
-
-      this.data = newObj;
-    },
-  },
+  methods: {},
   created() {},
-  mounted() {
-    this.getValues();
-  },
+  mounted() {},
+
 };
 </script>
 
